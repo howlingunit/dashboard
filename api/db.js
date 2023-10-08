4b1cb3245d33c49f8bce5849c7c8aab85a8f06c3
howlingunit <howlingunit3456@gmail.com>
2023-10-08 21:49:58 +0100

    feat!: working on db, have a basic insert

--- api/db.js
+++ api/db.js
@@ -1,18 +1,53 @@
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-const uri = "<connection string uri>";
+import { MongoClient } from "mongodb";
+
+
+const uri = "mongodb://localhost:27017/";
 const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+const db = 'dashboard_db'
+
+// temp vars
+const user = 'u1'
+
+export async function new_section(req, res) {
+
+  const database = client.db(db);
+  const userData = database.collection(user);
+
+  const section = {
+    name: 'test',
+    cards: {card1:{
+        title: 'card 1',
+        link: '',
+        text: 'this is a card yayy :)'
+      }}
+    
   }
+
+  const sectionID = await userData.insertOne(section)
+
+  await client.close();
+
+  res.json(sectionID)
+
+  //res 1: {"acknowledged":true,"insertedId":"652312f0caf75b3527aa29cc"}
+
 }
-run().catch(console.dir);
+
+export async function getUserdata(req, res) {
+  const database = client.db(db);
+  const userData = database.collection(user);
+
+
+
+  
+  const options = {
+    sort: { name: 1 },
+    projection: { _id: 0, name: 1, cards: 1 },
+  };
+
+  const sections = userData.findOne({name: 'test'}, options);
+
+  await client.close();
+
+  res.json(sections)
+}
--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import * as db from './db.js'
 
 
 const app = express();
 
+app.get('/testing', db.new_section);
+
+app.get('/getUserData', db.getUserdata);
 
 app.listen(8080);
